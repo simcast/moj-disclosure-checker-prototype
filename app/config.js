a707cea529b84fdb50f96944b4d4a93300e4d212
Simon Castillo <simon.castillo@digital.justice.gov.uk>
2020-01-27 12:48:36 +0000

    new version of multiples mvp

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Check when a caution or conviction is spent',
+  serviceName: '',
 
   // Default port that prototype runs on
   port: '3000',
